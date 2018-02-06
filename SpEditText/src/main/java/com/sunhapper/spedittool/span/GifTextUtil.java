23825d0f7771d056ed4ee40227ec0a7cf687bb59
sunhapper <877746899@qq.com>
2018-02-06 22:51:14 +0800

    add comment

--- SpEditText/src/main/java/com/sunhapper/spedittool/span/GifTextUtil.java
+++ SpEditText/src/main/java/com/sunhapper/spedittool/span/GifTextUtil.java
@@ -75,16 +75,19 @@ public class GifTextUtil {
 
   public static void setText(final TextView textView, final CharSequence nText,
       final BufferType type) {
+    //type 默认SPANNABLE，保证textView中取出来的是Spannable类型
     textView.setText(nText, type);
     CharSequence text = textView.getText();
     if (text instanceof Spannable) {
       ImageSpan[] gifSpans = ((Spannable) text).getSpans(0, text.length(), ImageSpan.class);
+      //将之前的Callback disable
       Object oldCallback = textView
           .getTag(R.id.drawable_callback_tag);
       if (oldCallback != null && oldCallback instanceof CallbackForTextView) {
         ((CallbackForTextView) oldCallback).disable();
       }
       Callback callback = new CallbackForTextView(textView);
+      //callback在drawable中是弱引用
       //让textview持有callback,防止callback被回收
       textView.setTag(R.id.drawable_callback_tag, callback);
       for (ImageSpan gifSpan : gifSpans) {
@@ -93,12 +96,13 @@ public class GifTextUtil {
           drawable.setCallback(callback);
         }
       }
+      //gifSpanWatcher是SpanWatcher,继承自NoCopySpan
+      //只有setText之后设置SpanWatcher才能成功
       ((Spannable) text).setSpan(gifSpanWatcher, 0, text.length(),
           Spanned.SPAN_INCLUSIVE_INCLUSIVE | Spanned.SPAN_PRIORITY);
 
     }
     textView.invalidate();
-
   }
 
 
