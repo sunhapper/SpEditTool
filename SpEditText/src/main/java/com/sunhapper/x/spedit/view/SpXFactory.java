81c8abdb681d5193d3e2946e9dad03ef855e098a
sunhapper <haipeng.sun3@56qq.com>
2019-01-29 17:48:18 +0800

    EmojiSpannableStringBuilder

--- SpEditText/src/main/java/com/sunhapper/x/spedit/view/SpXFactory.java
+++ SpEditText/src/main/java/com/sunhapper/x/spedit/view/SpXFactory.java
@@ -1,6 +1,8 @@
 package com.sunhapper.x.spedit.view;
 
+import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.Editable.Factory;
 import android.text.NoCopySpan;
@@ -14,13 +16,25 @@ import java.util.List;
  */
 public class SpXFactory extends Factory {
     private List<NoCopySpan> mNoCopySpans;
+    @Nullable
+    private static Class<?> sWatcherClass;
 
+
+    @SuppressLint("PrivateApi")
     public SpXFactory(List<NoCopySpan> watchers) {
         mNoCopySpans = watchers;
+        try {
+            String className = "android.text.DynamicLayout$ChangeWatcher";
+            sWatcherClass = this.getClass().getClassLoader().loadClass(className);
+        } catch (Throwable var2) {
+            ;
+        }
     }
 
     public Editable newEditable(@NonNull CharSequence source) {
-        SpannableStringBuilder spannableStringBuilder = SpannableStringBuilder.valueOf(source);
+        SpannableStringBuilder spannableStringBuilder = sWatcherClass != null ? EmojiSpannableStringBuilder.create(
+                sWatcherClass, source)
+                : SpannableStringBuilder.valueOf(source);
         for (NoCopySpan span : mNoCopySpans) {
             spannableStringBuilder.setSpan(span, 0, source.length(),
                     Spanned.SPAN_INCLUSIVE_INCLUSIVE | Spanned.SPAN_PRIORITY);
