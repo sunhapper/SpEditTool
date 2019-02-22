1f7e5da0da77ae8ed5a265bc4a0f0007f3aa1e50
sunhapper <haipeng.sun3@56qq.com>
2019-02-22 09:46:58 +0800

    update demo
    解决三星设备上IntegratedSpan完整性得不到保证的问题
    
    fix #7

--- app/src/main/java/me/sunhapper/spcharedittool/data/MentionUser.java
+++ app/src/main/java/me/sunhapper/spcharedittool/data/MentionUser.java
@@ -6,14 +6,17 @@ import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
 
+import com.sunhapper.x.spedit.mention.span.BreakableSpan;
 import com.sunhapper.x.spedit.mention.span.IntegratedSpan;
 
 /**
  * Created by sunhapper on 2019/1/30 .
+ * 使用三星输入法IntegratedSpan完整性不能保证，所以加上BreakableSpan使得@mention完整性被破坏时删除对应span
  */
-public class MentionUser implements IntegratedSpan, DataSpan {
+public class MentionUser implements IntegratedSpan, BreakableSpan, DataSpan {
     public String name;
     public long id;
+    private Object styleSpan;
 
     public MentionUser() {
         name = "sunhapper";
@@ -21,8 +24,9 @@ public class MentionUser implements IntegratedSpan, DataSpan {
     }
 
     public Spannable getSpanableString() {
+        styleSpan = new ForegroundColorSpan(Color.MAGENTA);
         SpannableString spannableString = new SpannableString(getDisplayText());
-        spannableString.setSpan(new ForegroundColorSpan(Color.MAGENTA), 0, spannableString.length(),
+        spannableString.setSpan(styleSpan, 0, spannableString.length(),
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         spannableString.setSpan(this, 0, spannableString.length(),
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
@@ -41,5 +45,18 @@ public class MentionUser implements IntegratedSpan, DataSpan {
                 "name='" + name + '\'' +
                 ", id='" + id + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean isBreak(Spannable text) {
+        int spanStart = text.getSpanStart(this);
+        int spanEnd = text.getSpanEnd(this);
+        boolean isBreak = spanStart >= 0 && spanEnd >= 0 && !text.subSequence(spanStart, spanEnd).toString().equals(
+                getDisplayText());
+        if (isBreak && styleSpan != null) {
+            text.removeSpan(styleSpan);
+            styleSpan = null;
+        }
+        return isBreak;
     }
 }
